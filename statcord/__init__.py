3987d6f6f87d6d14b9eed49efe9552d34e2386d0
Anish <anishanne1@gmail.com>
2020-04-30 20:53:50 -0400

    Update __init__.py

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'statcord.py'
 __author__ = 'statcord.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020, statcord.com'
-__version__ = '1.0.2'
+__version__ = '2.0.0'
 
 name = "statcord"
 
@@ -12,4 +12,4 @@ from .exceptions import *
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=1, minor=0, micro=2, releaselevel='final', serial=0)
+version_info = VersionInfo(major=2, minor=0, micro=0, releaselevel='final', serial=0)

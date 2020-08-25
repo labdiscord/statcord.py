604b116f00f613379794d4de40855299e19ff430
Anish <anishanne1@gmail.com>
2020-08-25 12:51:44 -0400

    Update __init__.py

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'statcord.py'
 __author__ = 'statcord.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020, statcord.com'
-__version__ = '3.0.1'
+__version__ = '3.0.2'
 
 name = "statcord"
 

a44ac5ad3cd0d06342a0be36879e6638b05fa9da
Iapetus-11 <olimdahacker@gmail.com>
2020-12-01 12:57:39 -0500

    use double quotes for consistency

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -1,8 +1,8 @@
-__title__ = 'statcord.py'
-__author__ = 'statcord.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2020, statcord.com'
-__version__ = '3.0.5'
+__title__ = "statcord.py"
+__author__ = "statcord.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020, statcord.com"
+__version__ = "3.0.5"
 
 name = "statcord"
 
@@ -10,5 +10,5 @@ from collections import namedtuple
 from .client import Client
 from .exceptions import *
 
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-version_info = VersionInfo(major=3, minor=0, micro=5, releaselevel='final', serial=0)
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=3, minor=0, micro=5, releaselevel="final", serial=0)
